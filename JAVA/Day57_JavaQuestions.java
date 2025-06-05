19ba04993e8647c738b92ccb992f83780ac19fb0
Krishna Chaitanya <krishnachaitanyamaddukuri4488@gmail.com>
2025-06-05 17:57:19 +0530

    Till Date

---
+++ JAVA/Day57_JavaQuestions.java
@@ -0,0 +1,75 @@
+/*
+
+Write a Java code to return the length of longest sub-string without repeating 
+characters
+
+Sample Input: 
+-------------
+abcabcbb
+
+Sample Output: 
+--------------
+3
+
+
+*/
+
+/*
+Problem: 
+--------
+Write a Java code to identify the nth largest number without 
+sorting the array
+
+
+Sample Input:
+-------------
+4 2
+3 1 5 2
+
+Sample Output:
+--------------
+3
+
+*/
+
+
+
+/*
+
+Problem: 
+Write Java code to identify if given two strings are Anagrams 
+(strings that contain same set of alphabets)
+
+Sample Input: 
+-------------
+listen silent
+
+Sample Output: 
+--------------
+true
+
+*/
+/*
+
+Write Java function to return minimum number of denominations – given an infinite
+supply of each denomination of {1, 2, 5, 10, 20, 50, 100, 200,500, 2000} and 
+a target value N
+
+Sample Input: 
+-------------
+187
+
+Sample Output: 
+--------------
+[100, 50, 20, 10, 5, 2]
+
+*/
+
+/*
+
+Write Java code to print system date & time in format like 
+2021-10-02 10:30:00 AM
+
+Sample Output: 2025-06-04 11:35:27 AM
+
+*/
